b179a7ef182fb88fee50436cb7c9000ba841cae4
DogukanAlyanak <mail@dogukanalyanak.com>
2024-11-19 09:40:38 +0300

    clipboard fix

--- resources/views/partials/room_invite_modal.blade.php
+++ resources/views/partials/room_invite_modal.blade.php
@@ -15,12 +15,14 @@
                         <div class="form-group">
                             <div class="input-group">
 
-                                <input id="inviteCodeInput" type="password" class="form-control" aria-label="Oda Davet Kodu" value="{{ $gameRoom->invite_code }}">
-                                <input id="inviteCodeInputCopy" type="hidden" value="{{ $gameRoom->invite_code }}">
+                                <input id="inviteCodeInput" type="password" class="form-control"
+                                    aria-label="Oda Davet Kodu" value="{{ $gameRoom->invite_code }}">
+                                {{-- <input id="inviteCodeInputCopy" type="hidden" value="{{ $gameRoom->invite_code }}"> --}}
 
                                 <button class="btn btn-primary show-invite-code" type="button"><i
                                         class="fa-solid fa-eye"></i></button>
-                                <button class="btn btn-primary copy-invite-code" type="button"><i
+                                <button class="btn btn-primary copy-invite-code"
+                                    data-copy="{{ $gameRoom->invite_code }}" type="button"><i
                                         class="fa-solid fa-copy"></i></button>
                             </div>
                         </div>
@@ -48,17 +50,48 @@
             }
         })
 
-        let timer
-        $(document).on('click', '.copy-invite-code', function() {
-            let copyText = $('#inviteCodeInputCopy');
-            copyText.select();
-            document.execCommand('copy');
-            $(`.alert-p`).html("Kopyalandı: " + copyText.val());
-            clearTimeout(timer)
-            timer = setTimeout(() => {
+        $(document).on(`click`, `.copy-invite-code`, function() {
+            const dataCopyValue = $(this).data('copy');
+            if (dataCopyValue) {
+                if (navigator.clipboard && navigator.clipboard.writeText) {
+                    navigator.clipboard.writeText(dataCopyValue).then(() => {
+                        copyAlert("Kopyalandı: " + dataCopyValue);
+                    }).catch(err => {
+                        copyAlert('Failed to copy text: ' + err.message);
+                    });
+                } else {
+                    copyToClipboardFallback(dataCopyValue);
+                }
+            } else {
+                copyAlert('No data-copy attribute found');
+            }
+        });
+
+
+        function copyToClipboardFallback(text) {
+            const tempInput = document.createElement('textarea');
+            tempInput.value = text;
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            try {
+                document.execCommand('copy');
+                copyAlert("Kopyalandı: " + text);
+            } catch (err) {
+                copyAlert('Fallback failed: ' + err.message);
+            }
+            document.body.removeChild(tempInput);
+        }
+
+
+        let copyTimer;
+        function copyAlert(text) {
+            console.log(text);
+            $(`.alert-p`).html(text);
+            clearTimeout(copyTimer);
+            copyTimer = setTimeout(() => {
                 $(`.alert-p`).html("");
             }, 5000);
-        });
+        }
     </script>
 @endpush
 
